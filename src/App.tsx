9597a5a6f0d7f49259007864bc26cd0fea7d8d7c
AlokGJ <git@stackblitz.com>
2026-02-02 17:01:54 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Gallery } from './Gallery';
+import './App.css';
+
+function App() {
+  return <Gallery />;
+}
+
+export default App;
---
+++ src/Gallery.tsx
@@ -0,0 +1,94 @@
+import { useState, useEffect, useRef, useCallback } from 'react';
+import './App.css';
+
+const useGallery = () => {
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const getItems = async (page: number): Promise<Array<unknown>> => {
+      const response = await fetch(
+        `https://picsum.photos/v2/list?page=${page}`,
+        { signal: controller.signal }
+      );
+
+      return await response.json();
+    };
+    getItems(currentPage).then((data) => {
+      setItems((prev) => [...prev, ...data]);
+    });
+  }, [currentPage]);
+
+  return {
+    items,
+    currentPage,
+    fetchNextPage: setCurrentPage,
+  };
+};
+
+const useInfiniteScroll = (callback: Function) => {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    let observer: IntersectionObserver | null = null;
+    if (scrollRef.current) {
+      observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              console.log('Interacting');
+              callback();
+            }
+          });
+        },
+        {
+          threshold: 0.25,
+        }
+      );
+
+      setTimeout(() => !!observer && observer.observe(scrollRef.current), 5000);
+    }
+
+    return () => {
+      !!observer && scrollRef.current && observer.unobserve(scrollRef.current);
+    };
+  }, []);
+
+  return scrollRef;
+};
+
+export const Gallery = () => {
+  const { items, fetchNextPage } = useGallery();
+  const onScroll = useCallback(() => {
+    console.log('Scrolled');
+    fetchNextPage((prev) => prev + 1);
+  }, []);
+  const scrollRef = useInfiniteScroll(onScroll);
+  return (
+    <div className="gallery-container">
+      {items?.map((item) => (
+        <GalleryItem item={item} />
+      ))}
+      <div ref={scrollRef} className="scroll-bottom" />
+    </div>
+  );
+};
+
+const GalleryItem = ({ item }) => {
+  return (
+    <div className="gallery-item">
+      <img src={item.download_url} alt="Gallery image not found" />
+      <span>{item.author}</span>
+    </div>
+  );
+};
+
+/**
+ ContextProvider
+  - <Context.Provider value={[list]} >
+    <Gallery />
+    <Paginator />
+    
+
+ */
